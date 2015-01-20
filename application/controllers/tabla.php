7452d0eea47b094577aa97b42dac19ca4cf8e664
Leonardo Montilla <leonardomo84@gmail.com>
2015-01-20 14:41:39 +0100

    Los filtros se mantienen en los input-value

--- application/controllers/tabla.php
+++ application/controllers/tabla.php
@@ -2,9 +2,65 @@
 
 class Tabla extends CI_Controller
 {
+	private $where = "where ";
+
   public function index(){
+  	$tmpl = array('table_open' => '<table border="1" style="margin:auto">');
+
+  	$where = ($this->input->post('filtrar')) ? crear_where() : "";
+
     $data['res'] = $this->Inmueble->carga_inmuebles();
+
+		$this->table->set_template($tmpl);
 
     $this->load->view('index', $data);
   }
+
+  private function crear_where(){
+  	$lavavajillas = $this->load->post('lavavajillas');
+  	$trastero = $this->load->post('trastero');
+  	$garaje = $this->load->post('garaje');
+  	$premin = $this->load->post('premin');
+  	$premax = $this->load->post('premax');
+  	$hab = $this->load->post('hab');
+  	$ban = $this->load->post('ban');
+
+  	$where .= ($lavavajillas !== FALSE) ? comprobar_where()."lavavajillas is true " : "";
+    $where .= ($garaje !== FALSE) ? comprobar_where()."garaje is true " : "";
+    $where .= ($trastero !== FALSE) ? comprobar_where()."trastero is true " : "";
+
+    if((isset($premin) && $premin != "") && (isset($premax) && $premax != "")){
+      $where .= comprobar_where()."precio between $premin and $premax ";
+    }
+
+    if((isset($premin) && $premin != "") && (isset($premax) && $premax == "")){
+      $where .= comprobar_where()."precio >= $premin ";
+    }
+
+    if((isset($premin) && $premin == "") && (isset($premax) && $premax != "")){
+      $where .= comprobar_where()."precio <= $premax ";
+    }
+
+    if((isset($hab) && $hab != "")){
+      $where .= comprobar_where()."habitaciones >= $hab";
+    }
+
+    if((isset($ban) && $ban != "")){
+      $where .= comprobar_where()."baños >= $ban";
+    }
+  }
+
+  private function comprobar_where(){
+    if($where != "where ")
+      $where .= "and ";
+  }
+
+  /*Lavavajillas <input type="checkbox" name="lavavajillas" <?=(isset($lavavajillas) && $lavavajillas == "on") ? "checked" : "" ?> > <br>
+      Garaje <input type="checkbox" name="garaje" <?=(isset($garaje) && $garaje == "on") ? "checked" : "" ?> > <br>
+      Trastero <input type="checkbox" name="trastero" <?=(isset($trastero) && $trastero == "on") ? "checked" : "" ?> > <br>
+      Precio Mínimo <input type="text" name="premin" value=<?=(isset($premin) && $premin != "") ? "$premin" : "" ?> > <br>
+      Precio Máximo <input type="text" name="premax" value=<?=(isset($premax) && $premax != "") ? "$premax" : ""  ?> > <br>
+      Número minimo de habitaciones <input type="text" name="hab" value=<?=(isset($hab) && $hab != "") ? "$hab" : ""  ?> > <br>
+      Número mínimo de baños  <input type="text" name="ban" value=<?=(isset($ban) && $ban != "") ? "$ban" : ""  ?> > <br>
+      <br>*/
 }
--- application/helpers/index_helper.php
+++ application/helpers/index_helper.php
@@ -3,25 +3,25 @@
 function filtros(){
   $out  = form_open('tabla', array('align' => 'center'));
   $out .=   form_label('Lavavajillas', 'lavavajillas');
-  $out .=     form_checkbox('lavavajillas');
+  $out .=     form_checkbox('lavavajillas', null, comprobar_check('lavavajillas'));
   $out .=   '<br />';
   $out .=   form_label('Garaje', 'garaje');
-  $out .=     form_checkbox('garaje');
+  $out .=     form_checkbox('garaje', null, comprobar_check('garaje'));
   $out .=   '<br />';
   $out .=   form_label('Trastero', 'trastero');
-  $out .=     form_checkbox('trastero');
+  $out .=     form_checkbox('trastero', null, comprobar_check('trastero'));
   $out .=   '<br />';
   $out .=   form_label('Precio Mínimo', 'premin');
-  $out .=     form_input('premin');
+  $out .=     form_input('premin', comprobar_value('premin'));
   $out .=   '<br />';
   $out .=   form_label('Precio Máximo', 'premax');
-  $out .=     form_input('premax');
+  $out .=     form_input('premax', comprobar_value('premax'));
   $out .=   '<br />';
   $out .=   form_label('Número mínimo de habitaciones', 'hab');
-  $out .=     form_input('hab');
+  $out .=     form_input('hab', comprobar_value('hab'));
   $out .=   '<br />';
   $out .=   form_label('Número mínimo de baños', 'ban');
-  $out .=     form_input('ban');
+  $out .=     form_input('ban', comprobar_value('ban'));
   $out .=   '<br />';
   $out .=   form_submit('filtrar', 'Filtrar');
   $out .= form_close();
@@ -30,19 +30,19 @@ function filtros(){
   return $out;
 }
 
+function comprobar_value($nombre){
+  $CI =& get_instance();
+  $valor = $CI->input->post($nombre);
+
+  return ($valor !== FALSE) ? $valor : "";
+}
+
+function comprobar_check($nombre){
+  $CI =& get_instance();
+  return ($CI->input->post($nombre) !== FALSE) ? TRUE : FALSE;
+}
+
 function tabla_inm($res){
   $CI =& get_instance();
   return $CI->table->generate($res);
 }
-
-    /*<form action="index.php" method="get" align="center">
-      Lavavajillas <input type="checkbox" name="lavavajillas" <?=(isset($lavavajillas) && $lavavajillas == "on") ? "checked" : "" ?> > <br>
-      Garaje <input type="checkbox" name="garaje" <?=(isset($garaje) && $garaje == "on") ? "checked" : "" ?> > <br>
-      Trastero <input type="checkbox" name="trastero" <?=(isset($trastero) && $trastero == "on") ? "checked" : "" ?> > <br>
-      Precio Mínimo <input type="text" name="premin" value=<?=(isset($premin) && $premin != "") ? "$premin" : "" ?> > <br>
-      Precio Máximo <input type="text" name="premax" value=<?=(isset($premax) && $premax != "") ? "$premax" : ""  ?> > <br>
-      Número minimo de habitaciones <input type="text" name="hab" value=<?=(isset($hab) && $hab != "") ? "$hab" : ""  ?> > <br>
-      Número mínimo de baños  <input type="text" name="ban" value=<?=(isset($ban) && $ban != "") ? "$ban" : ""  ?> > <br>
-      <br>
-      <input type="submit" value="Filtrar">
-    </form>*/
